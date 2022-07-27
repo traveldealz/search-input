f2f115d3d272deb7cb5d654085ad635704946bf5
Johannes Kinast <5927337+undefined@users.noreply.github.com>
2022-07-27 14:57:54 +0000

    Add Keyword Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import AirportSearch from './airport-component.js';
 import DestinationSearch from './destination-component.js';
+import KeywordSearch from './keyword-component.js';
 import HotelSearch from './hotel-component.js';
 import RailwaystationSearch from './railwaystation-component.js';
 
 window.customElements.define('airport-search', AirportSearch );
 window.customElements.define('destination-search', DestinationSearch );
+window.customElements.define('keyword-search', KeywordSearch );
 window.customElements.define('hotel-search', HotelSearch );
 window.customElements.define('railwaystation-search', RailwaystationSearch );
--- src/index.stories.js
+++ src/index.stories.js
@@ -8,5 +8,6 @@ export default {
 
 export const airportSearch = () => `<airport-search><input></airport-search>`;
 export const destinationSearch = () => `<destination-search><input></destination-search>`;
+export const keywordSearch = () => `<keyword-search endpoint="https://cors-anywhere.herokuapp.com/https://travel-dealz.de/wp-json/wp/v2/search"><input></keyword-search>`;
 export const hotelSearch = () => `<hotel-search><input><input name="location_type"><input name="location_city"><input name="location_countrycode"><input name="location_coordinates"></hotel-search>`;
 export const railwaystationSearch = () => `<railwaystation-search><input><input name="location_id"><input name="location_type"><input name="location_city"><input name="location_state"><input name="location_countrycode"><input name="location_coordinates"></railwaystation-search>`;
