80e894d7f038833ba629d34ed4530c4149432010
Johannes Kinast <5927337+undefined@users.noreply.github.com>
2025-02-27 16:05:33 +0000

    define only if not already defined

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import KeywordSearch from './keyword-component.js';
 import HotelSearch from './hotel-component.js';
 import RailwaystationSearch from './railwaystation-component.js';
 
-window.customElements.define('airport-search', AirportSearch );
-window.customElements.define('destination-search', DestinationSearch );
-window.customElements.define('keyword-search', KeywordSearch );
-window.customElements.define('hotel-search', HotelSearch );
-window.customElements.define('railwaystation-search', RailwaystationSearch );
+if(undefined === window.customElements.get('airport-search')) {
+  window.customElements.define('airport-search', AirportSearch);
+}
+if(undefined === window.customElements.get('destination-search')) {
+  window.customElements.define('destination-search', DestinationSearch);
+}
+if(undefined === window.customElements.get('keyword-search')) {
+  window.customElements.define('keyword-search', KeywordSearch);
+}
+if(undefined === window.customElements.get('hotel-search')) {
+  window.customElements.define('hotel-search', HotelSearch);
+}
+if(undefined === window.customElements.get('railwaystation-search')) {
+ window.customElements.define('railwaystation-search', RailwaystationSearch ); 
+}
